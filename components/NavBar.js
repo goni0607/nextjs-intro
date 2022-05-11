561484806aa032ee4678c30e34f1a944c2283a6a
triplek(vm) <triplek@triplek.net>
2022-05-11 16:23:03 +0900

    2022-05-11 last study

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Image } from "next/image";
 import styles from './NavBar.module.css';
 
 export default function NavBar() {
@@ -7,6 +8,7 @@ export default function NavBar() {
     
     return (
         <nav>
+            <img src="/vercel.svg" />
             <ul>
                 <li>
                     <Link href="/">
@@ -19,15 +21,47 @@ export default function NavBar() {
                     </Link>
                 </li>
             </ul>
+            <style>
+            </style>
             <style jsx>{`
-                nav {
-                    background-color: tomato;
-                }
                 a {
                     text-decoration: none;
                 }
+                nav {
+                    display: flex;
+                    gap: 10px;
+                    flex-direction: column;
+                    align-items: center;
+                    padding-top: 20px;
+                    padding-bottom: 10px;
+                    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+                }
+                nav ul {
+                    margin: 0;
+                    padding: 0;
+                    display: flex;
+                    gap: 10px;
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
+                }
+                nav ul li {
+                    list-style-type: none;
+                }
+                img {
+                    max-width: 100px;
+                    margin-bottom: 5px;
+                }
+                nav a {
+                    font-weight: 600;
+                    font-size: 18px;
+                }
                 .active {
-                    color: yellow;
+                    color: tomato;
+                }
+                nav div {
+                    display: flex;
+                    gap: 10px;
                 }
             `}</style>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,82 @@
+import Link from "next/link";
+// import { useState, useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 import Seo from "../components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
+    /* Client Side Renderring
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        (async () => {
+            // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+            // const json = await response.json();
+            const { results } = await(
+                await fetch(`/api/movies`)
+            ).json();
+            setMovies(results);
+        })();
+    }, [])
+    */
 
-    // const [counter, setCounter] = useState(0);
-
+    /* Example for router hook
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(
+            {
+                pathname: `/movies/${id}`,
+                query: {
+                    title,
+                },
+            },
+            `/movies/${id}`
+        );
+    };
+    */
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(`/movies/${title}/${id}`);
+    };
+    
     return (
-        <div>
+        <div className="container">
             <Seo title="Home" />
-            <h1>Welcome, NextJS!!</h1>
+            {!results && <h4>Loading...</h4>}
+            {results?.map((movie) => (
+                <div key={movie.id} className="movie">
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} onClick={() => onClick(movie.id, movie.original_title)} />
+                    <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+                        <a>
+                            {movie.original_title}
+                        </a>
+                    </Link>
+                </div>
+            ))}
+            <style jsx>{`
+                .container {
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    padding: 20px;
+                    gap: 20px;
+                }
+                .movie {
+                    display: block;
+                    cursor: pointer;
+                }
+                .movie img {
+                    max-width: 100%;
+                    border-radius: 12px;
+                    transition: transform 0.2s ease-in-out;
+                    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+                }
+                .movie:hover img {
+                    transform: scale(1.05) translateY(-10px);
+                }
+                .movie a {
+                    font-size: 18px;
+                    font-weight: bold;
+                    text-align: center;
+                }
+            `}</style>
         </div>
         /*
         <div>
@@ -17,4 +86,16 @@ export default function Home() {
         </div>
         */
     );
+}
+
+
+export async function getServerSideProps() {
+    const { results } = await (
+        await fetch(`http://localhost:3000/api/movies`)
+    ).json();
+    return {
+        props: {
+            results,
+        },
+    };
 }
